933a7e431699dba0d728ca4159e22adf2a209f15
BuildTools <unconfigured@null.spigotmc.org>
2022-09-29 16:25:15 +0200

    9618430 - 2022-09-29 16:25:14

--- NameHelper.cs
+++ NameHelper.cs
@@ -1,9 +1,13 @@
 public static class NameHelper // TypeDefIndex: 12464
 {
+	private static Dictionary<string, string> _cache; 
+
 
 	public static string Get(ulong userId, string name) { }
 
 	public static string Get(IPlayerInfo playerInfo) { }
+
+	private static void .cctor() { }
 
 }
 
