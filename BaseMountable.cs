e2c39e4e93e2e6fbd0d69a01c81f0ac296458e4f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-09 01:59:35 +0100

    10279562 - 2023-01-09 01:59:35

--- BaseMountable.cs
+++ BaseMountable.cs
@@ -173,9 +173,6 @@ public class BaseMountable : BaseCombatEntity // TypeDefIndex: 10051
 	private static void .cctor() { }
 
 	[CompilerGeneratedAttribute]
-	private bool <ValidDismountPosition>g__HitOurself|70_0(RaycastHit hit) { }
-
-	[CompilerGeneratedAttribute]
 	private void <DoPlayerMovement>
 
 }
